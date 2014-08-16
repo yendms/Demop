ca9a738cabb6a4bdaefde9d19a372c59f31f7b8e
Yen Adams Sokama-Neuyam <yen@Scholarsup.com>
2014-08-16 17:55:12 +0200

    Finish Static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Demop'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Demop')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Prayer for prayer lovers | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Prayer for prayer lovers | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Prayer for prayer lovers | About")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Prayer for prayer lovers | Contact")
+    end
+  end
+end
