bfe363cbb19fd72d77f1e288c2e50b2f11f5acad
Yen Adams Sokama-Neuyam <yen@Scholarsup.com>
2014-08-21 13:47:21 +0200

    Signin and Signout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to PrayersUp!"
       redirect_to @user
       # Handle a successful save.
